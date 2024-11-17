382bc192fbcabe3b5c161819881761b9bf4db33c
Pieter Tjerkstra <pietertjerkstra@gmail.com>
2024-11-17 13:21:58 +0100

    mappad aangepast

--- start_app.R
+++ start_app.R
@@ -1,8 +1,9 @@
 library(plumber)
 # 'plumber.R' is the location of the file shown above
-pr("/home/rstudio/plumber.R") %>%
+pr("/home/rstudio/app-main/plumber.R") %>%
   pr_run(port=8000, host = "0.0.0.0")
 
 
 #run in terminal:
+cd app-main
 #R CMD BATCH start_app.R
